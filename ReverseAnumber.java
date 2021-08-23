b713b1699f17d6f5f4fe6f8fb191739a79f2584f
Zedan Saheer <zed.saheer5@gmail.com>
2021-08-23 12:02:01 +0530

    fixed logic with reverse a number program

--- ReverseAnumber.java
+++ ReverseAnumber.java
@@ -6,10 +6,12 @@ public class ReverseAnumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for(int i = 0; i<=n ; i++){
+        int ans = 0;
+       while(0<n){
             int rem = n%10;
             n= n/10;
-            System.out.print(rem);
+            ans = ans * 10 + rem;
         }
+        System.out.print(ans);
     }
 }
