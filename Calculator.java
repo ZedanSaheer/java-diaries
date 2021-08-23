e20025da98d9b289255f0a9d90c689f8be241ed4
Zedan Saheer <zed.saheer5@gmail.com>
2021-08-23 14:11:26 +0530

    completed all logics for calculator

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,8 @@ public class Calculator {
         Scanner in = new Scanner(System.in);
 
         while(true){
-            System.out.println("Please Select Operator :");
-            char op = in.next().trim().charAt(0);
+            System.out.println("Please Select Operator : \n * \n % \n + \n - \n /  \n If you want to exit this application press X or x!");
+            char op = in.nextLine().trim().charAt(0);
             if(op == '+'|| op == '/'|| op == '%'|| op == '*'|| op == '-'){
                 // input two numbers
                 System.out.println("Enter First Number :");
@@ -36,6 +36,12 @@ public class Calculator {
                     System.out.println("The Answer is :");
                     System.out.println(a % b);
                 }
+            }
+            else if (op == 'x' || op == 'X'){
+                    break;
+                }
+            else{
+                System.out.println("Invalid Operator , Sorry!");
             }
         }
     }
