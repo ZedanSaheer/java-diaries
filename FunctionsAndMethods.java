68e9a5aceffd66b9f40b9829bf3c81ef67c5c556
Zedan Saheer <zed.saheer5@gmail.com>
2021-08-24 17:35:57 +0530

    Different function options , trying to get used to important function rules

--- FunctionsAndMethods.java
+++ FunctionsAndMethods.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class FunctionsAndMethods {
     public static void main(String[] args) {
-        sum();
+       int ans = sumWithParams(10,32);
+        System.out.println(ans);
+    }
+
+    static int sumWithReturn(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a = in.nextInt();
+        System.out.println("Enter another number");
+        int b = in.nextInt();
+        int sum = a + b;
+        return sum;
     }
 
     static void sum(){
@@ -15,5 +26,10 @@ public class FunctionsAndMethods {
         int b = in.nextInt();
         int sum = a + b;
         System.out.println("Your answer is" + " " + sum);
+    }
+
+    static int sumWithParams(int a, int b){
+        int sum = a+b;
+        return sum;
     }
 }
